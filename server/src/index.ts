f19a4d678edb55faa48daeb01c93b068c60001b2
Asher Hwang <dev@hsh.kr>
2021-08-24 10:15:25 +0900

    Add error handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 import createConnection from './db/createConnection';
 import startExpressServer from './express';
 
-createConnection().then(async (db) => {
-  await Promise.all([
-    startExpressServer(db).then(() => {
+function onReadyServerHasError(error: Error) {
+  console.log('-----------------');
+  console.error(error);
+  process.exit();
+}
+
+createConnection().then((db) => {
+  console.clear();
+  console.log('db connect success!');
+
+  startExpressServer(db)
+    .then(() => {
       console.log('express server start!');
-    }),
-  ]);
+    })
+    .catch(onReadyServerHasError);
 });
